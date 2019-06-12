085c8fec3626b7d23585cdb1db7e407e727a45fb
Aaron Lisman <alisman@cloudflare.com>
2019-06-12 15:24:57 -0500

    still broken, gonna rewrite

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  let response = await fetch('http://httpbin.org/image/jpeg')
+  const providedUrl = new URL(request.url).searchParams.get('url')
+  const url = providedUrl ? providedUrl : 'http://httpbin.org/image/jpeg'
+  let response = await fetch(url)
 
   let { readable, writable } = new TransformStream()
-  let newResponse = new Response(readable, { status: 200 })
+  let newResponse = new Response(readable, response)
   modifyJPGStream(response.body, writable)
   return newResponse
-  // return new Response(readable, response)
 }
--- jpgstream.js
+++ jpgstream.js
@@ -1,5 +1,3 @@
-import { parseExif } from 'exif-reader'
-
 export async function modifyJPGStream(readable, writable) {
   let offset = 0
   let pending = 0
@@ -9,7 +7,7 @@ export async function modifyJPGStream(readable, writable) {
   let state = 'ff'
   let started = false
   let dqtSeq = 0
-  let width= -1, height = -1
+  let width = -1, height = -1
 
   const reader = readable.getReader();
   const writer = writable.getWriter();
@@ -18,18 +16,22 @@ export async function modifyJPGStream(readable, writable) {
     for (; ;) {
       const { done, value } = await reader.read();
 
+      console.log(state, done, value)
+
       if (done) {
-        flushMarker.call(this, state, buffers)
+        console.log('done')
+        await flushMarker.call(this, state, buffers)
         break
       }
 
       var j = 0
       for (var i = 0; i < value.length; i++) {
         var b = value[i]
+        // console.log(hexb(b))
         if (state === 'data') {
           if (b === 0xff) {
             buffers.push(value.slice(j, i))
-            state = flushMarker.call(this, state, buffers)
+            state = await flushMarker.call(this, state, buffers)
             buffers = []
             j = i
             state = 'code'
@@ -42,7 +44,7 @@ export async function modifyJPGStream(readable, writable) {
           buffers.push(value.slice(i, i + n))
           pending -= n
           if (pending === 0) {
-            state = flushMarker.call(this, state, buffers)
+            state = await flushMarker.call(this, state, buffers)
             if (state === 'data') j = i
             buffers = []
           }
@@ -50,8 +52,11 @@ export async function modifyJPGStream(readable, writable) {
           pos += n
           continue
         }
+        console.log('state', state, 'pos', pos)
+        console.log('writing', hexb(b))
+        await writer.write(value.slice(i, i + 1))
         if (state === 'ff' && b !== 0xff) {
-          return next(new Error('expected 0xff, received: ' + hexb(b)))
+          throw new Error('expected 0xff, received: ' + hexb(b))
         } else if (state === 'ff') {
           state = 'code'
         } else if (state === 'code') {
@@ -62,7 +67,6 @@ export async function modifyJPGStream(readable, writable) {
           } else if (b === 0xd8) { // SOI
             started = true
             state = 'ff'
-            write(writer, { type: 'SOI', start: pos - 1, end: pos + 1 })
           } else if (b === 0xe0) { // JF{IF,XX}-APP0
             state = 'app0'
           } else if (b === 0xda) { // SOS
@@ -85,16 +89,20 @@ export async function modifyJPGStream(readable, writable) {
             state = 'sos'
           } else if (b === 0xfe) { // ???
             state = '0xfe'
+          } else if (b === 0xee) { // ???
+            state = '0xee'
+          } else if (b === 0xed) { // ???
+            state = '0xed'
           } else {
-            return next(new Error('unknown code: ' + hexb(b)))
+            throw new Error('unknown code: ' + hexb(b))
           }
         } else if (state === 'app0') {
           if (offset === 0) s1 = b
           else if (offset === 1) s2 = b
           else if (offset === 2 && b !== 0x4a) {
-            return next(new Error('in app0 expected 0x4a, received: ' + hexb(b)))
+            throw new Error('in app0 expected 0x4a, received: ' + hexb(b))
           } else if (offset === 3 && b !== 0x46) {
-            return next(new Error('in app0 expected 0x46, received: ' + hexb(b)))
+            throw new Error('in app0 expected 0x46, received: ' + hexb(b))
           } else if (offset === 4 && b === 0x49) {
             state = 'jfif-app0'
             offset = -1
@@ -102,8 +110,8 @@ export async function modifyJPGStream(readable, writable) {
             state = 'jfxx-app0'
             offset = -1
           } else if (offset >= 4) {
-            return next(new Error(
-              'in app0 expected 0x49 or 0x58, received: ' + hexb(b)))
+            throw new Error(
+              'in app0 expected 0x49 or 0x58, received: ' + hexb(b))
           }
           offset++
         } else if (state === 'jfif-app0') {
@@ -120,157 +128,84 @@ export async function modifyJPGStream(readable, writable) {
         pos++
       }
       if (state === 'data') {
+        console.log('pushing buffer')
         buffers.push(value.slice(j, i))
       }
       if (pos > 2 && !started) {
         throw new Error('start of image not found')
       } else {
-        break // was calling next()??? idk
+        continue
       }
     }
+
+    console.log('closing')
+    await writer.close()
   } catch (e) {
     console.log(e)
   }
 
-  function flushMarker(state, buffers) {
-    var buf = buffers.length === 1 ? buffers[0] : Buffer.concat(buffers)
+  async function flushMarker(state, buffers) {
+    var buf = buffers.length === 1 ? buffers[0] : Uint8Array.from(Array.prototype.concat(...buffers.map(a => Array.from(a))));
     const dv = new DataView(buf.buffer)
+    console.log('flushing', state, buf.length, 'bytes')
     if (state === 'data') {
-      write(writer, {
-        type: 'DATA',
-        start: pos - 2,
-        end: pos + buf.length,
-        data: buf
-      })
-    } else if (state === 'jfif-app0') {
-      var units = 'unknown'
-      if (buf[2] === 0) units = 'aspect'
-      else if (buf[2] === 1) units = 'pixels per inch'
-      else if (buf[2] === 2) units = 'pixels per cm'
-      write(writer, {
-        type: 'JFIF',
-        start: pos - 9,
-        end: pos + buf.length,
-        version: buf[0] + '.' + buf[1], // major.minor
-        density: {
-          units: units,
-          x: dv.getUint16(3),
-          y: dv.getUint16(5)
-        },
-        thumbnail: {
-          width: buf[7],
-          height: buf[8],
-          data: buf.slice(9, 9 + 3 * buf[7] * buf[8])
+      // mess with it
+      await writer.write(buf)
+    } else {
+      if (state !== 'sos') {
+        await writer.write(buf)
+      }
+      if (state === 'jfif-app0') {
+        var units = 'unknown'
+        if (buf[2] === 0) units = 'aspect'
+        else if (buf[2] === 1) units = 'pixels per inch'
+        else if (buf[2] === 2) units = 'pixels per cm'
+      } else if (state === 'jfxx-app0') {
+        var thumb = {
+          format: 'unknown',
+          width: 0,
+          height: 0,
+          data: null
         }
-      })
-    } else if (state === 'jfxx-app0') {
-      var thumb = {
-        format: 'unknown',
-        width: 0,
-        height: 0,
-        data: null
-      }
-      if (buf[0] === 0x10) {
-        thumb.format = 'JPEG'
-        thumb.data = buf.slice(1)
-      } else if (buf[0] === 0x11) {
-        thumb.format = 'PAL'
-        thumb.width = buf[1]
-        thumb.height = buf[2]
-        thumb.palette = buf.slice(3, 3 + 768)
-        thumb.data = buf.slice(3 + 768 + thumb.width * thumb.height)
-      } else if (buf[0] === 0x12) {
-        thumb.format = 'RGB'
-        thumb.width = buf[1]
-        thumb.height = buf[1]
-        thumb.data = 3 * thumb.width * thumb.height
-      }
-      write(writer, {
-        type: 'JFXX',
-        start: pos - 9,
-        end: pos + buf.length,
-        thumbnail: thumb
-      })
-    } else if (state === 'app1') {
-      var row = parseExif(buf)
-      row.type = 'EXIF'
-      row.start = pos - 2
-      row.end = pos + buf.length
-      write(writer, row)
-    } else if (state === 'app2') {
-      write(writer, {
-        type: 'FPXR',
-        start: pos - 2,
-        end: pos + buf.length
-      })
-    } else if (state === 'dqt') {
-      var tables = []
-      for (var i = 1; i < buf.length; i += 0x41) {
-        if (buf[i - 1] !== dqtSeq++) {
-          return this.emit('error', new Error('unexpected DQT byte at ' +
-            (offset + i - 1) + ' (' + i + '): ' + buf[i - 1]))
+        if (buf[0] === 0x10) {
+          thumb.format = 'JPEG'
+          thumb.data = buf.slice(1)
+        } else if (buf[0] === 0x11) {
+          thumb.format = 'PAL'
+          thumb.width = buf[1]
+          thumb.height = buf[2]
+          thumb.palette = buf.slice(3, 3 + 768)
+          thumb.data = buf.slice(3 + 768 + thumb.width * thumb.height)
+        } else if (buf[0] === 0x12) {
+          thumb.format = 'RGB'
+          thumb.width = buf[1]
+          thumb.height = buf[1]
+          thumb.data = 3 * thumb.width * thumb.height
         }
-        tables.push(buf.slice(i, i + 0x40))
+      } else if (state === 'app1') {
+      } else if (state === 'app2') {
+      } else if (state === 'dqt') {
+        var tables = []
+        for (var i = 1; i < buf.length; i += 0x41) {
+          if (buf[i - 1] !== dqtSeq++) {
+            return this.emit('error', new Error('unexpected DQT byte at ' +
+              (offset + i - 1) + ' (' + i + '): ' + buf[i - 1]))
+          }
+          tables.push(buf.slice(i, i + 0x40))
+        }
+      } else if (state === 'dht') {
+      } else if (state === 'dri') {
+      } else if (state === 'sos') {
+        console.log(buf)
+        return 'data'
+      } else if (state === 'sof') {
+        width = dv.getUint16(3)
+        height = dv.getUint16(1)
+      } else if (state === 'eoi') {
       }
-      write(writer, {
-        type: 'DQT',
-        start: pos - 2,
-        end: pos + buf.length,
-        tables: tables
-      })
-    } else if (state === 'dht') {
-      write(writer, {
-        type: 'DHT',
-        start: pos - 2,
-        end: pos + buf.length,
-        data: buf
-      })
-    } else if (state === 'dri') {
-      write(writer, {
-        type: 'DRI',
-        start: pos - 2,
-        end: pos + buf.length
-      })
-    } else if (state === 'sos') {
-      write(writer, {
-        type: 'SOS',
-        start: pos - 2,
-        end: pos + buf.length
-      })
-      return 'data'
-    } else if (state === 'sof') {
-      width = dv.getUint16(3)
-      height = dv.getUint16(1)
-      write(writer, {
-        type: 'SOF',
-        start: pos - 2,
-        end: pos + buf.length,
-        precision: buf[0],
-        width: width,
-        height: height,
-        H0: Math.floor(buf[7] / 16),
-        V0: buf[7] % 16
-      })
-    } else if (state === 'eoi') {
-      write(writer, {
-        type: 'EOI',
-        start: pos - 2,
-        end: pos + buf.length,
-      })
+      return 'ff'
     }
-    return 'ff'
   }
-}
-
-function write(writer, obj) {
-  const encoder = new TextEncoder();
-  writer.write(
-    encoder.encode(
-      JSON.stringify(
-        obj
-      )
-    )
-  )
 }
 
 function hexb(n) { return '0x' + (n < 0x10 ? '0' : '') + n.toString(16) }
